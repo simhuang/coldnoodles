0663c8524b029d0073aa9740ff598e1df3ca12c3
Simon Huang <simhuang92@gmail.com>
2020-03-28 00:26:12 -0400

    create separate firebase config file

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import * as firebase from "firebase";
+import firebaseConfig from "../../firebase.json";
 
 import reducers from "../reducers";
 
@@ -11,27 +12,13 @@ import MainContainer from "./MainContainer";
 
 class App extends React.Component {
   componentDidMount() {
-    console.log("hello");
-    // Your web app's Firebase configuration
-    var firebaseConfig = {
-      apiKey: "AIzaSyBFBxGwurpGs-YDkgNg1H2Y7W_uLHi8ZKc",
-      authDomain: "coldnoodles-6a047.firebaseapp.com",
-      databaseURL: "https://coldnoodles-6a047.firebaseio.com",
-      projectId: "coldnoodles-6a047",
-      storageBucket: "coldnoodles-6a047.appspot.com",
-      messagingSenderId: "94617644493",
-      appId: "1:94617644493:web:db069eabc511a351c46884",
-      measurementId: "G-N771JYHSJC"
-    };
-    // Initialize Firebase
+    /**
+     * This code initialize the firebase server. Make
+     * sure a firebase instance is created and that firebase.json
+     * contains all the proper credentials
+     */
     firebase.initializeApp(firebaseConfig);
     firebase.analytics();
-
-    const firestore = firebase.firestore();
-    firestore
-      .collection("set")
-      .doc("0")
-      .set({ name: "hello" });
   }
   render() {
     return (
